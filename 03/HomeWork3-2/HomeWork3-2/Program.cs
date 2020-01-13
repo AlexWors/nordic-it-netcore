3b5e9caa4aeea66c0aaffa6b3cb55b2b534b85cb
unknown <bushidokon@gmail.com>
2020-01-13 21:25:07 +0300

    added difficult part of homework

---
+++ 03/HomeWork3-2/HomeWork3-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[10, 10];
+            for(int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    mas[i, j] = i * j;
+                    Console.Write(mas[i, j]);
+                }
+                Console.WriteLine("\n"); 
+                
+            }
+            
+        }
+    }
+}
