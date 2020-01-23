e089a078790c56cba8bc5185a12f03ca683c18b1
unknown <bushidokon@gmail.com>
2020-01-23 17:10:19 +0300

    Home work of the 5 lesson added

---
+++ 05/HomeWork5/HomeWork5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        public enum geometricFigures
+        {
+            Circle = 1,
+            Triangle = 2,
+            Rectangle = 3
+        }
+        static void Main(string[] args)
+        {
+            double area = 0;
+            double perimeter = 0;
+
+            Console.WriteLine("Enter type of figure: ");
+            Console.WriteLine("1 - Circle \n2 - Triangle \n3 - Rectangle");
+            int input = 0;
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your entered wrong data!");
+            }
+            
+            int figures = (int)(geometricFigures)input;
+            try
+            {
+                switch (figures)
+                {
+                    case (int)geometricFigures.Circle:
+                        Console.WriteLine("Enter circle radius: ");
+                        double radius = double.Parse(Console.ReadLine());
+                        if (radius <= 0)
+                            throw new Exception("The value should be more than 0!");
+                        area = Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+                        perimeter = Math.Round(2 * Math.PI * radius, 2);
+                        break;
+
+                    case (int)geometricFigures.Triangle:
+                        Console.WriteLine("Enter the side of the triangle: ");
+                        double side = double.Parse(Console.ReadLine());
+                        if (side <= 0)
+                            throw new Exception("The value should be more than 0!");
+                        area = Math.Round(Math.Pow(side, 2) * Math.Sqrt(3) / 4, 2);
+                        perimeter = Math.Round(side * 3, 2);
+                        break;
+
+                    case (int)geometricFigures.Rectangle:
+                        Console.WriteLine("Enter the hight of the rectangle: ");
+                        double height = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the width of the rectangle ");
+                        double width = double.Parse(Console.ReadLine());
+                        if (height <= 0 || width <= 0)
+                            throw new Exception("The value should be more than 0!");
+                        area = Math.Round(height * width, 2);
+                        perimeter = Math.Round((2 * height) + (2 * width), 2);
+                        break;
+                    default:
+                        throw new Exception("Figure not defined!");
+                        
+
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Your entered wrong data!");
+            }
+            
+
+                Console.WriteLine($"Area of your figure = {area}");
+                Console.WriteLine($"Perimeter of your figure = {perimeter}");
+        
+        
+        }
+    }
+}
--- 06/ClassWork6/ClassWork6/Program.cs
+++ 06/ClassWork6/ClassWork6/Program.cs
@@ -6,57 +6,57 @@ namespace ClassWork6
     {
         static void Main(string[] args)
         {
-            //string str;
-            //byte numberOfLetters = 0;
-            //Console.WriteLine("Enter string: ");
-            //do
-            //{
-            //    str = Console.ReadLine();
-            //    if (str == "exit")
-            //        continue;
-            //    numberOfLetters += (byte)str.Length;
-            //    Console.WriteLine($"number of entered numbers: {numberOfLetters}");
-
-            //}
-            //while (str != "exit");
-
-            //Console.WriteLine("Your entered " + str);
-            //Console.ReadKey();
-
-
-
-             var marks = new[]
-             {
-                new [] { 2, 3, 3, 2, 3}, // Monday (it was a good weekend :)
-                new [] { 2, 4, 5, 3},    // Tuesday (anyway better than Monday)
-                null,                    // Wednesday (felt sick, stayed at home :( )
-                new [] { 5, 5, 5, 5},    // Thursday (God mode :)
-                new [] { 4 }             // Friday (a very short day)
-             };
-
-            int marksCount = 0;
-            int totalSum = 0;
-            for (int j = 0; j < marks.Length; j++)
+            string str;
+            byte numberOfLetters = 0;
+            Console.WriteLine("Enter string: ");
+            do
             {
-                int sum = 0;
-                if (marks[j] == null)
-                {
-                    Console.WriteLine("The average mark for day #3: N/A");
+                str = Console.ReadLine();
+                if (str == "exit")
                     continue;
-                }
+                numberOfLetters += (byte)str.Length;
+                Console.WriteLine($"number of entered numbers: {numberOfLetters}");
 
-
-                for (int i = 0; i < marks[j].Length; i++)
-                {
-                    sum += marks[j][i];
-                    totalSum += marks[j][i];
-                    marksCount++;
-
-                }
-
-                Console.WriteLine($"The average mark for day #{j}: {(float)sum / marks[j].Length}");
             }
-            Console.WriteLine($"The average mark for week: {(float)totalSum / marksCount}");
+            while (str != "exit");
+
+            Console.WriteLine("Your entered " + str);
+            Console.ReadKey();
+
+
+
+            // var marks = new[]
+            // {
+            //    new [] { 2, 3, 3, 2, 3}, // Monday (it was a good weekend :)
+            //    new [] { 2, 4, 5, 3},    // Tuesday (anyway better than Monday)
+            //    null,                    // Wednesday (felt sick, stayed at home :( )
+            //    new [] { 5, 5, 5, 5},    // Thursday (God mode :)
+            //    new [] { 4 }             // Friday (a very short day)
+            // };
+
+            //int marksCount = 0;
+            //int totalSum = 0;
+            //for (int j = 0; j < marks.Length; j++)
+            //{
+            //    int sum = 0;
+            //    if (marks[j] == null)
+            //    {
+            //        Console.WriteLine("The average mark for day #3: N/A");
+            //        continue;
+            //    }
+
+
+            //    for (int i = 0; i < marks[j].Length; i++)
+            //    {
+            //        sum += marks[j][i];
+            //        totalSum += marks[j][i];
+            //        marksCount++;
+
+            //    }
+
+            //    Console.WriteLine($"The average mark for day #{j}: {(float)sum / marks[j].Length}");
+            //}
+            //Console.WriteLine($"The average mark for week: {(float)totalSum / marksCount}");
 
 
 
