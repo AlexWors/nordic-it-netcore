d6e81c5e987b1ce821e4d82a4477581f7ec5cef5
Smithprojects <anton29141@gmail.com>
2020-01-25 14:07:50 +0300

    class Work of the 7 lesson added

---
+++ 07/ClassWork7/ClassWork7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            string resultMultiplication = firstNumber + " * " + secondNumber + " = " + Math.Round(firstNumber * secondNumber, 2);
+            Console.WriteLine(resultMultiplication);
+
+            string resultAddition = string.Format("{0} + {1} + {2:#.##}", firstNumber, secondNumber, firstNumber + secondNumber);
+            Console.WriteLine(resultAddition);
+
+            string resultSubstraction = $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber:#.##}";
+            Console.WriteLine(resultSubstraction);
+
+        }
+    }
+}
---
+++ 07/ClassWork7_1/ClassWork7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassWork7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Enter underString: ");
+            string underStr = Console.ReadLine();
+            int index = 0;
+            string indexes = string.Empty; 
+            do
+            {
+                index = str.IndexOf(underStr, index);
+                if (index < 0)
+                break;
+                
+                indexes += index + " ";
+                index++;
+            }
+            while (true);
+
+            if(string.IsNullOrEmpty(indexes))
+            {
+                Console.WriteLine("Serching string is not found!");
+            }
+            else
+            Console.WriteLine("indexes substrings in string: " + indexes);
+        }
+    }
+}
