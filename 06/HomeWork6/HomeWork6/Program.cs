b692fb45ea77738e105c73f9a32fe09371e0ce2b
unknown <bushidokon@gmail.com>
2020-01-28 21:23:28 +0300

    Corrections to the 1 home work added

--- 06/HomeWork6/HomeWork6/Program.cs
+++ 06/HomeWork6/HomeWork6/Program.cs
@@ -6,20 +6,33 @@ namespace HomeWork6
     {
         static void Main(string[] args)
         {
-            int number;
-            string strNumber;
+            int number = 0;
+            string strNumber = string.Empty;
             Console.WriteLine("Enter a natural number of not more than 2 billion: ");
             do
             {
-                strNumber = Console.ReadLine();
-                if (int.TryParse(strNumber, out number))
-                    break;
-                Console.WriteLine("You entered wrong data! Try again");
-            } while (true);
+                try
+                {
+                    strNumber = Console.ReadLine();
+                    number = int.Parse(strNumber);
+                    if(number <= 0)
+                    {
+                        Console.WriteLine("Number must be greater than zero!");
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("You entered a number greater than 2 billion!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You entered wrong data! Try again");
+                }
+            } while (number <= 0);
             int evenDigits = 0;
             for(int i = 0; i < strNumber.Length; i++)
             {
-                var num = int.Parse(i.ToString());
+                var num = int.Parse(strNumber[i].ToString());
                 if(num % 2 == 0)
                 {
                     evenDigits++;
