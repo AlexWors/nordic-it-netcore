d4c5df0a8e3881e7193075a1cdbe566634798dec
Smithprojects <anton29141@gmail.com>
2020-01-29 22:10:06 +0300

    Class Work of the 8 lesson added

---
+++ 08/ClassWork8/ClassWork8/Program.cs
@@ -0,0 +1,36 @@
+﻿    using System;
+using System.Collections.Generic;
+
+namespace ClassWork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //или var
+            List<int> intList = new List<int>();
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+
+            Console.WriteLine(string.Join(", ", intList));
+
+            var listOfStrings = new List<string>();
+            listOfStrings.Add("one");
+            listOfStrings.Add("one");
+            listOfStrings.Add("one");
+            listOfStrings.Add("one");
+
+            Console.WriteLine(string.Join(", ", listOfStrings));
+
+            var list = new List<int>
+            {
+                1,
+                2,
+                3
+            };
+
+        }
+    }
+}
---
+++ 08/ClassWork8_1/ClassWork8_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWork8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<double> numbers = new Queue<double>();
+            Console.WriteLine("Введите числа для извлечения квадратного корня: ");
+
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if(input == "run")
+                {
+                    while (numbers.Count > 0)
+                    {
+                        double result = numbers.Dequeue();
+                        Console.WriteLine(Math.Sqrt(result));                         
+                    }
+
+                    continue;
+                }                
+                else if(input == "exit")
+                {
+                    Console.WriteLine($"Завершение работы. Число оставшихся задач {numbers.Count}");
+                    break;
+                }
+
+                numbers.Enqueue(double.Parse(input));
+                
+            }
+
+        }
+    }
+}
