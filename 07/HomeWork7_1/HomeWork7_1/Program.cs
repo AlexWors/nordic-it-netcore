5216f8a5975c57f97b6207fa9ba0e7b2edd6eca1
unknown <bushidokon@gmail.com>
2020-01-31 22:36:16 +0300

    Second part of 7 home work added

---
+++ 07/HomeWork7_1/HomeWork7_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while (string.IsNullOrEmpty(input))
+            {
+                try
+                {
+                    Console.WriteLine("Введите непустую строку: ");
+                    input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        throw new Exception("Вы ввели пустую строку :( Попробуйте ещё раз: ");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            char[] inputReverse = input.ToCharArray();
+            Array.Reverse(inputReverse);
+            input = new string(inputReverse);
+            Console.WriteLine(input.ToLower());
+        }
+    }
+}
