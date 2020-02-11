1d17db3ce8a9eebc5df5bd1b08e555062c11f549
unknown <bushidokon@gmail.com>
2020-02-11 16:13:01 +0300

    Homework of the 10 lesson added

---
+++ 10/HomeWork10/HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int personCount = 3;
+            var personArray = new person[personCount];
+
+            for(int i = 0; i < personArray.Length; i++)
+            {
+                try
+                {
+                    var person = new person();
+                    Console.WriteLine($"Введите имя{i + 1}: ");
+                    person.Name = Console.ReadLine();
+                    Console.WriteLine($"Введите возраст{i + 1}: ");
+                    person.Age = int.Parse(Console.ReadLine());
+                    personArray[i] = person;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    i--;
+                }
+            }
+            for(int i = 0; i < personArray.Length; i++)
+            {
+                Console.WriteLine(personArray[i].Output);
+            }
+            Console.WriteLine("Для выхода нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 10/HomeWork10/HomeWork10/HomeWork10/person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork10
+{
+    class person
+    {
+        private string _name;
+        private int _age;
+        
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(value == string.Empty)
+                {
+                    throw new Exception("Введена пустая строка!");
+                }
+                _name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Вораст не может быть отрицательным!");
+                }
+                _age = value;
+            }
+        }
+
+        public int AgeAfterFourYears
+        {
+            get { return Age + 4; }
+        }
+
+        public string Output
+        {
+            get
+            {
+                return $"Имя: {Name}. Возраст через 4 года: {AgeAfterFourYears}";
+            }
+        }
+    }
+}
